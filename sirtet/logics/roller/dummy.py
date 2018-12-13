0fb7123f1aa236cfd6fdd2ece1ec45e18c3e1834
Jose Carlos Recuero Arias <jose.recuero@gmail.com>
2018-12-13 07:20:14 -0800

    Add stats

--- sirtet/logics/roller/dummy.py
+++ sirtet/logics/roller/dummy.py
@@ -1,6 +1,26 @@
 from sirtet.logics.roller.jobs import Job
 
 
+class Stat:
+    def __init__(self, name: str, maxi: int, init: int = -1, own: int = -1):
+        self.name: str = name
+        self.val: int = init if init != -1 else maxi
+        self.max: int = maxi
+        self.own: int = own if own != -1 else maxi
+        self.points: int = 0
+        self.next: int = 1
+        self.level: int = 0
+
+    def add_points(self, points: int = 1) -> bool:
+        if points > self.points + self.next:
+            return False
+        self.points += points
+        if self.points == self.next:
+            self.level += 1
+            self.next *= 2
+        return True
+
+
 class Dummy:
     def __init__(
         self,
@@ -13,16 +33,17 @@ class Dummy:
     ):
         self.name: str = name
         self.description: str = ""
-        self.damage: int = damage
-        self.life: int = life
-        self.__max_life: int = life
-        self.skill: int = 0
-        self.__max_skill: int = skill
+        self.damage: Stat = Stat("damage", damage)
+        self.life: Stat = Stat("life", life, 0)
+        self.skill: Stat = Stat("skill", skill, 0)
+        self.defense: Stat = Stat("defense", 0)
         self.job = job
-        self.__player: bool = player
+        self._player: bool = player
+        self.xp: int = 0
+        self.level: int = 1
 
     def get_damage(self, damage: int) -> int:
-        return damage * self.job.damage * self.damage
+        return damage * self.job.damage * self.damage.val
 
     def get_life(self, life: int) -> int:
         return life * self.job.life
@@ -31,19 +52,19 @@ class Dummy:
         return skill * self.job.skill
 
     def damaged(self, damage: int) -> "Dummy":
-        self.life -= damage
+        self.life.val -= damage
         return self
 
     def healed(self, heal: int) -> "Dummy":
-        self.life += heal
-        if self.life > self.__max_life:
-            self.life = self.__max_life
+        self.life.val += heal
+        if self.life.val > self.life.max:
+            self.life.val = self.life.max
         return self
 
     def skilled(self, skill: int) -> "Dummy":
-        self.skill += skill
-        if self.skill > self.__max_skill:
-            self.skill = self.__max_skill
+        self.skill.val += skill
+        if self.skill.val > self.skill.max:
+            self.skill.val = self.skill.max
         return self
 
     def __str__(self) -> str:
@@ -51,14 +72,14 @@ class Dummy:
             self.name,
             self.job.__class__.__name__,
             chr(9825),
-            self.life,
-            self.__max_life,
+            self.life.val,
+            self.life.max,
             self.job.life,
             chr(9733),
-            self.skill,
-            self.__max_skill,
+            self.skill.val,
+            self.skill.max,
             self.job.skill,
-            chr(9784) if self.__player else chr(9763),
-            self.damage,
+            chr(9784) if self._player else chr(9763),
+            self.damage.val,
             self.job.damage,
         )
